ce10eb89fc06b9a09c3827dc6ddbbc5ceb8119c2
heidless <rob.lockhart@yahoo.co.uk>
2024-02-02 11:27:46 +0000

    project setup

---
+++ src/pages/dashboard/data/Data.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Data = () => {
+    return (
+        <h1>Data Test</h1>
+    )
+}
+
+export default Data;
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <h1>Login Test</h1>
+    )
+}
+
+export default Login;
